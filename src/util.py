4ecb19bef795524e98ba4324d4362e7edba16592
Dylan <rattledew17@gmail.com>
2025-04-02 22:59:42 -0700

    added frame and label

--- src/util.py
+++ src/util.py
@@ -5,6 +5,14 @@ OPTION_COLOR = "#404040"
 FONT_COLOR = "#f5f5f5"
 
 class Util():
+    """
+    Creates widgets.
+    """
+    
+    def frame(window) -> Frame:
+        return Frame(window,
+                     bg=BG_COLOR,)
+    
     def button(window) -> Button:
         return Button(window,
                       font=("Helvetica", 15, "bold"),
@@ -12,3 +20,9 @@ class Util():
                       bg=OPTION_COLOR,
                       padx=30,
                       pady=15,)
+        
+    def label(window) -> Label:
+        return Label(window,
+                     font=("Helvetica", 15, "bold"),
+                     fg=FONT_COLOR,
+                     bg=BG_COLOR,)
