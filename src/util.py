33354a099befc60eef099dcff90c2f0b26a435d1
Dylan <rattledew17@gmail.com>
2025-04-03 22:58:47 -0700

    added active-color modifcation

--- src/util.py
+++ src/util.py
@@ -2,7 +2,10 @@ from tkinter import *
 
 BG_COLOR = "#2b2b2c"
 OPTION_COLOR = "#404040"
+HOVER_COLOR = "#7c7c7c"
 FONT_COLOR = "#f5f5f5"
+
+FONT = "Helvetica"
 
 class Util():
     """
@@ -15,14 +18,16 @@ class Util():
     
     def button(window) -> Button:
         return Button(window,
-                      font=("Helvetica", 15, "bold"),
+                      font=(FONT, 15, "bold"),
                       fg=FONT_COLOR,
                       bg=OPTION_COLOR,
+                      activeforeground=FONT_COLOR,
+                      activebackground=OPTION_COLOR,
                       padx=30,
                       pady=15,)
         
     def label(window) -> Label:
         return Label(window,
-                     font=("Helvetica", 15, "bold"),
+                     font=(FONT, 15, "bold"),
                      fg=FONT_COLOR,
                      bg=BG_COLOR,)
