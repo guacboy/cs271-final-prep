a538ef6bcdd747c82f7416f1e76acaca8cdd4dcc
Dylan <rattledew17@gmail.com>
2025-04-12 10:29:54 -0700

    added shuffle choices (forgot to add)

--- src/bank.py
+++ src/bank.py
@@ -1,27 +1,26 @@
 import random
 
-def some_tag() -> None:
-    # TODO: randomize choices
-    print("some tag")
-
-def another_tag() -> None:
-    print("another tag")
+def on_shuffle_choices(choices: list) -> list:
+    random.shuffle(choices)
+    return choices
 
 question_bank = {
     "Module 1": {
         "some tag": {
             "Q1": {
-                "question": "some question",
+                "question": "|",
                 "answer": "some answer",
-                "function": lambda: some_tag(),
+                "function": lambda: on_shuffle_choices([
+                    "a", "b", "c", "d"]),
                 "format": "radio",
             },
         },
         "another some tag": {
             "Q2": {
-                "question": "another some question",
+                "question": "|",
                 "answer": "another some answer",
-                "function": lambda: some_tag(),
+                "function": lambda: on_shuffle_choices([
+                    "a", "b", "c", "d"]),
                 "format": "radio",
             },
         },
@@ -29,9 +28,10 @@ question_bank = {
     "Module 2": {
         "another tag": {
             "Q1": {
-                "question": "another question",
+                "question": "|",
                 "answer": "another answer",
-                "function": lambda: another_tag(),
+                "function": lambda: on_shuffle_choices([
+                    "a", "b", "c", "d"]),
                 "format": "radio",
             },
         },
